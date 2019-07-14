95199681e775728efd28ab2f61976ccd67cd293b
Summer <summer.d.howard@gmail.com>
2019-07-14 16:49:19 -0500

    initial commit

---
+++ Assets/PlayerScript.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+    public float speed = 10.0F;
+
+    void Update()
+    {
+      var move = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
+      transform.position += move * speed * Time.deltaTime;   
+    }
+
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+	    Debug.Log("xxxxx");
+
+        if (other.tag == "Dino")
+        {
+             transform.position = new Vector3(0,0,0);
+        }    
+    }
+
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+Simple scene - walk into dino, reset position
+
+Shows how to move a gameobject, animations, and the use of triggers with tags.
